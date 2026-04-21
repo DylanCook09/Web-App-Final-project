8a829701850e978f4d2f44682166954921106cb3
Eli-Ba <s1006974@student.knoxschools.org>
2026-04-21 17:39:05 +0000

    changes

--- database.py
+++ database.py
@@ -7,20 +7,16 @@ def get_db():
 
 def init_db():
     conn = get_db()
-
     conn.executescript("""
-        CREATE TABLE IF NOT EXISTS users (
-            username TEXT PRIMARY KEY,
-            password TEXT
-        );
-        
-        CREATE TABLE IF NOT EXISTS user (
-            weight_lifted INTEGER,
-            reps INTEGER,
-            mile_time TEXT
-        );
+    CREATE TABLE IF NOT EXISTS entries (
+        created_on TEXT DEFAULT CURRENT_DATE PRIMARY KEY,
+        mile_time INT,
+        weight_lifted INT,
+        calories_consumed INT
+    );
     """)
     conn.commit()
     conn.close()
 
-init_db()
+if __name__ == "__main__":
+    init_db()
