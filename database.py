21b713ed49ea841282f3416687149b5ceea8f288
Eli-Ba <s1006974@student.knoxschools.org>
2026-04-21 17:51:19 +0000

    might work?

--- database.py
+++ database.py
@@ -1,24 +1,34 @@
 import sqlite3
 
 def get_db():
-    conn = sqlite3.connect("users.db")
-    conn.row_factory = sqlite3.Row
+    conn = sqlite3.connect('users.db')
+    conn.row_factory = sqlite3.Row 
+    return conn
+
+def get_entries_db():
+    conn = sqlite3.connect('entries.db')
+    conn.row_factory = sqlite3.Row 
     return conn
 
 def init_db():
     conn = get_db()
+
     conn.executescript("""
-        CREATE TABLE IF NOT EXISTS users(
-            username TEXT PRIMARY KEY,
-            password TEXT
-        );
-        CREATE TABLE IF NOT EXISTS entries (
-            created_on TEXT DEFAULT CURRENT_DATE PRIMARY KEY,
-            mile_time INT,
-            weight_lifted INT,
-            calories_consumed INT
-        );
+    CREATE TABLE IF NOT EXISTS users(
+        username TEXT PRIMARY KEY,
+        password TEXT
+    );
     """)
+
+    conn.executescript("""
+    CREATE TABLE IF NOT EXISTS entries (
+        created_on TEXT DEFAULT CURRENT_DATE PRIMARY KEY,
+        mile_time INT,
+        weight_lifted INT,
+        calories_consumed INT
+    );
+    """)
+    
     conn.commit()
     conn.close()
 
