8efccf4370d2997c843203faf0e318506a2dae84
DylanCook09 <s1073734@student.knoxschools.org>
2026-05-14 15:58:52 +0000

    agggg

--- app.py
+++ app.py
@@ -39,7 +39,8 @@ def login():
 
         if user and bcrypt.checkpw(password.encode("utf-8"), user["password"]):
             session["user"] = username
-            return redirect(url_for("dashboard"))
+            return re
+            direct(url_for("dashboard"))
         else:
             error = "Incorrect username or password"
 
